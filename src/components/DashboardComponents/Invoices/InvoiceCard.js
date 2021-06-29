076c729b942897563d1095d72bcb3367f7a7f969
loretta-arineva <wowshifter@gmail.com>
2021-06-29 21:40:45 +0100

    Formatted invoice price

--- src/components/DashboardComponents/Invoices/InvoiceCard.js
+++ src/components/DashboardComponents/Invoices/InvoiceCard.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 const InvoiceCard = ({ invoice }) => {
     const statusClasses = `status__container invoice--${invoice.status}`;
+
+    // Format invoice total price
+    var formatter = new Intl.NumberFormat('en-GB', {
+        style: 'currency',
+        currency: 'GBP',
+    });
+
     return (
         <div className="invoice-item" role="invoice-item">
             <h2>
@@ -10,7 +17,7 @@ const InvoiceCard = ({ invoice }) => {
             </h2>
             <p className="name">{invoice.clientName}</p>
             <p className="date">Due {invoice.createdAt}</p>
-            <p className="price">&#163;{invoice.total}</p>
+            <p className="price">{formatter.format(invoice.total)}</p>
             <div className={statusClasses}>
                 <div className="invoice-status__circle"></div>
                 <div className="invoice-status">{invoice.status}</div>
