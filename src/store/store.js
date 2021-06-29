60cbd4ff7d2a429d15b4a651cdd5b225d0b9de22
loretta-arineva <wowshifter@gmail.com>
2021-06-29 17:36:42 +0100

    Dashboard header basic styles added

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
-// import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import filterSlice from '../reducers/filterReducer';
 
-// const store = configureStore({
-//     reducer: {
+const store = configureStore({
+    reducer: {
+        filter: filterSlice.reducer
+    }
+});
 
-//     }
-// });
-
-// export default store;
+export default store;
