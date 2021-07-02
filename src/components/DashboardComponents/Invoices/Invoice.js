9babd4a6dee4d5be09d1cb6e2d8a30f4b5d9f43f
ivaniuss <ivan.emi94@gmail.com>
2021-07-01 22:57:29 -0500

    invoice header

--- src/components/DashboardComponents/Invoices/Invoice.js
+++ src/components/DashboardComponents/Invoices/Invoice.js
@@ -1,20 +1,53 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
-
+import { useHistory } from 'react-router-dom'
+import InvoiceStatus from './InvoiceStatus';
 const Invoice = () => {
 
-    const {id} = useParams()
- 
+    const history = useHistory();
+    const handleBack = () =>{
+        history.push('/invoice-app')
+    }
+
     return (
-        <div>
-            <div>
-                <div>{id}</div>
-                <div>Go back</div>
+        <div className = 'dashboard__container invoice-view'>
+            <div className = 'go-back' onClick={handleBack}>
+                <span className = 'invoice__arrow left-arrow'></span>
+                <span>Go back</span>
+            </div>
+            <div className='invoice-header'>
+                {/* <div> */}
+                <h6>Status</h6>
+                <InvoiceStatus  invoice={({status:'paid'})}/>
+                {/* </div> */}
+                {/* <div>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div> */}
             </div>
             <div>
+                content
             </div>
         </div>
     )
 }
 
 export default Invoice
+
+
+// <div className="invoice-preview__container">
+//     <div className="go-back">
+//         Your code. This is a separate component
+//     </div>
+//     <div className="invoice-preview__header">
+//         <div className="left">
+//             <p>Status</p>
+//             <p>Status component goes here</p>
+//         </div>
+//         <div className="right">
+//            Your buttons
+//         </div>
+//         <div>
+//             The invoice data
+//         </div>
+//     </div>
+// </div>
